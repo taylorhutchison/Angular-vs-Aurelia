15d057830c52e1f91deac6c04f96555e792d76b8
Adam Sheaffer <JJ06896@JJ0000M301211.local>
2016-06-11 11:33:22 -0500

    remove unneeded module; fix template typo

--- angular2/app/main.ts
+++ angular2/app/main.ts
@@ -1,5 +1,4 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
-import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+bootstrap(AppComponent);
